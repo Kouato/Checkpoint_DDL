48b8124d5c11a936aa6167171ae8a81141ad15f1
Kouato <bankouato@gmail.com>
2025-02-06 18:21:58 +0000

    Mise à jour du fichier

--- Point_de_controle_DataStructurePython.py
+++ Point_de_controle_DataStructurePython.py
@@ -1,57 +1,59 @@
-shopping_list = [] 
-n = ""
+shopping_list = []  
 compteur = 0
-element = ""
 
-print("######## Bienvenue à Shopping List ######## \n")
+print("######## Bienvenue à Shopping List ######## ")
 while True:
-    print("Quel est l'action que vous souhaitez effectuer ? \n")
-    print("1) Ajouter : ")
-    print("2) Afficher : ")
-    print("3) Supprimez : ")
-    print("4) Quittez :  \n")
+    print("\nQuel est l'action que vous souhaitez effectuer ?")
+    print("1) Ajouter un article")
+    print("2) Afficher la liste")
+    print("3) Supprimer un article")
+    print("4) Quitter\n")
 
-    reponse2 = input()
+    reponse2 = input(" Choisissez une option (1-4) : ")
     while reponse2 not in {"1", "2", "3", "4"}:
-        reponse2 = input("Entrez le format requis svp : ")
+        reponse2 = input(" Format invalide ! Entrez une option valide (1-4) : ")
 
-    if reponse2 == "1":
-        for i in range(0,10):
-            while True :
-                n = input("Entrez votre article svp (lettres uniquement) : \n")
-                if n.isalpha() :
-                   shopping_list.append(n)
-                   print(f" {n} a bien été ajoutez \n")
-                   compteur += 1
-                   break
-                    
-                else :
-                   print("Erreur , entrez le format demander ")
-        else:
-            print("Vous avez atteint le nombre maximum d'ajout.")
-    
-    elif reponse2 == "2":
-    
-        if shopping_list:
-            print("Vous avez sélectionné les éléments suivants :\n")
-            for element in shopping_list:
-                print(element)
-        else:
-            print("Votre liste est vide.\n")
-    
-    elif reponse2 == "3":
-        if shopping_list:
+    if reponse2 == "1":  # Ajouter un article
+        for i in range(0,5):
             while True:
-                n = input("Quel est le nom de l'élément que vous voulez supprimer ? ")
-                if n in shopping_list:
-                    shopping_list.remove(n)
-                    print(f"Vous avez supprimé {n} de votre shopping list.\n")
+                n = input("Entrez votre article (lettres uniquement) ou tapez 0 pour revenir au menu : ")
+                if n == "0":  
+                    print(" Retour au menu principal.")
+                    break
+                elif n.isalpha():
+                    shopping_list.append(n)
+                    print(f" '{n}' ajouté à votre liste !")
                     break
                 else:
-                    print("Désolé, cet article ne figure pas parmi vos articles.\n")
-        else:
-            print("Votre liste est vide, il n'y a rien à supprimer.")
-    
+                    print(" Erreur : Entrez un nom valide (sans chiffres ni caractères spéciaux) !")
     else:
+        print(" Vous avez atteint le nombre maximum d'ajouts.")
+
+    elif reponse2 == "2":  # Afficher la liste
+        print("\n Votre liste d'achats :")
+        if shopping_list:
+            for element in shopping_list:
+                print(f"- {element}")
+        else:
+            print("Votre liste est vide.")
+
+    elif reponse2 == "3":  # Supprimer un article
+        if shopping_list:
+            while True:
+                n = input("Quel élément supprimer ? (ou tapez 0 pour revenir au menu) : ")
+                if n == "0":
+                    print(" Retour au menu principal.")
+                    break
+                elif n in shopping_list:
+                    shopping_list.remove(n)
+                    print(f" '{n}' a été supprimé de votre liste.")
+                    break
+                else:
+                    print(" Cet article n'est pas dans la liste.")
+        else:
+            print(" Votre liste est vide, rien à supprimer.")
+
+    else:  # Quitter le programme
         print("Merci d'avoir utilisé ### Shopping List ###. À bientôt !")
         break
+
